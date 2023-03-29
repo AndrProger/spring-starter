395986b0595194e33d03607e9ac56c3d29083a46
stari <starinenko2251@gmail.com>
2023-03-29 17:12:51 +0600

    les-6

--- src/main/java/ApplicationRunner.java
+++ src/main/java/ApplicationRunner.java
@@ -3,19 +3,12 @@ import com.dmdev.spring.database.pool.ConnectionPool;
 import com.dmdev.spring.database.repository.CompanyRepository;
 import com.dmdev.spring.database.repository.UserRepository;
 import com.dmdev.spring.ioc.Container;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        Container container = new Container();
-//        ConnectionPool connectionPool = new ConnectionPool();
-//        UserRepository userRepository = new UserRepository(connectionPool);
-//        CompanyRepository companyRepository = new CompanyRepository(connectionPool);
-//        UserService userService = new UserService(userRepository, companyRepository);
-
-//        ConnectionPool connectionPool = container.get(ConnectionPool.class);
-//        UserRepository userRepository =  container.get(UserRepository.class);
-//        CompanyRepository companyRepository = container.get(CompanyRepository.class);
-        UserService userService = container.get(UserService.class);
-        //  todo: 27.03.2023 userService
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+        ConnectionPool connectionPool = context.getBean(ConnectionPool.class);
+        System.out.println(connectionPool);
     }
 }
--- src/main/java/com/dmdev/spring/database/pool/ConnectionPool.java
+++ src/main/java/com/dmdev/spring/database/pool/ConnectionPool.java
@@ -1,4 +1,21 @@
 package com.dmdev.spring.database.pool;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
 public class ConnectionPool {
+    private  final String username;
+    private  final Integer poolSize;
+
+    private  final List<String> args;
+
+    private  final Map<String,String> properties;
+
+    public ConnectionPool(String username, Integer poolSize, List<String> args, Map<String, String> properties) {
+        this.username = username;
+        this.poolSize = poolSize;
+        this.args = args;
+        this.properties = properties;
+    }
 }
