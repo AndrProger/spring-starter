9ead20630ee678052fe81f52374ed66cbd63e3b4
stari <starinenko2251@gmail.com>
2023-04-03 02:32:03 +0600

    13.-Custom-BeanFactoryPostProcessor

---
+++ src/main/java/com/dmdev/spring/bfpp/LogBeanFactoryPostProcessor.java
@@ -0,0 +1,20 @@
+package com.dmdev.spring.bfpp;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.PriorityOrdered;
+import org.springframework.core.annotation.Order;
+
+public class LogBeanFactoryPostProcessor implements BeanFactoryPostProcessor, PriorityOrdered {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        System.out.println();
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.LOWEST_PRECEDENCE;
+    }
+}
