040ba2523d7b35ea454690acf5cf4bf5ae8b5578
stari <starinenko2251@gmail.com>
2023-04-03 17:57:40 +0600

    16.-Custom-BeanPostProcessor.-Часть-1

--- src/main/java/com/dmdev/spring/database/pool/ConnectionPool.java
+++ src/main/java/com/dmdev/spring/database/pool/ConnectionPool.java
@@ -1,16 +1,18 @@
 package com.dmdev.spring.database.pool;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
 public class ConnectionPool {
-    private  final String username;
-    private  final Integer poolSize;
+    private final String username;
+    private final Integer poolSize;
 
-    private  final List<String> args;
+    private final List<String> args;
 
-    private   Map<String,String> properties;
+    private Map<String, String> properties;
 
     public ConnectionPool(String username, Integer poolSize, List<String> args, Map<String, String> properties) {
         this.username = username;
@@ -23,10 +25,12 @@ public class ConnectionPool {
         this.properties = properties;
     }
 
+    @PostConstruct
     private void init() {
         System.out.println("Init connection pool");
     }
 
+    @PreDestroy
     private void destroy() {
         System.out.println("Clean connection pool");
     }
